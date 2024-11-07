8ca50fb90352d1170735bafc9a9440c44caf9ac8
Luka de Jong <lukadejong2002@gmail.com>
2024-11-07 09:49:18 +0100

    Update portscan.lua

--- parsers/nmap/portscan.lua
+++ parsers/nmap/portscan.lua
@@ -1,7 +1,7 @@
 function(input)
 
     local lunajson = require("lunajson")
-    local decoded_input = lunajson.decode(input)[1]
+    local decoded_input = lunajson.decode(lunajson.decode(input)[1])
 
     local output = {}
     for ip, protocols in pairs(decoded_input) do
