db934f788164dcf2d402fa631d66e9fccad6e121
Luka de Jong <52010222+NekoLuka@users.noreply.github.com>
2024-11-12 10:43:04 +0100

    Created first parser and project layout (#1)
    * Created parser layout
    
    * Wrote nmap portscan parser
    
    * Create .gitignore
    
    * Update portscan.lua
    
    * Update portscan.lua
    
    ---------
    
    Co-authored-by: Bavo <bavo.famknol@gmail.com>

---
+++ parsers/nmap/portscan.lua
@@ -0,0 +1,16 @@
+function(input)
+
+    local lunajson = require("lunajson")
+    local decoded_input = lunajson.decode(lunajson.decode(input)[1])
+
+    local output = {}
+    for ip, protocols in pairs(decoded_input) do
+        for protocol, ports in pairs(protocols) do
+            for port, name in pairs(ports) do
+                table.insert(output, {short = port, long = table.concat({name.name, ip, port, protocol}, ",")})
+            end
+        end
+    end
+
+    return output
+end
